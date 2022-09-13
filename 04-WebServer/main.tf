72b07e97f07fb9cc38283e7cd292ae2a8d86599d
hyorch <hyorch@gmail.com>
2022-09-13 16:57:23 +0200

    subnet id from tfstate

--- 04-WebServer/main.tf
+++ 04-WebServer/main.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "webserver" {
   ami      = data.aws_ami.ubuntu.id # Latest Ubuntu 20.04
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.sg-webserver.id]  
-  subnet_id = "subnet-0ef5e6f0e925bb4df"
+  #subnet_id = "subnet-0ef5e6f0e925bb4df"
+  subnet_id = data.terraform_remote_state.vpc_state.outputs.vpc_nets_id[0].id
   key_name = var.ssh_key_name # SSH Key
 
   user_data       = <<-EOF
